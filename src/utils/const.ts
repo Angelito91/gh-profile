19b40640f8e6a664a8c80e9a15c9dd8dd47db63a
Angelito91 <portuondoangel@gmail.com>
2024-03-23 20:23:33 -0400

    feat: delete consts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,3 @@
-export const URL_USER = "https://api.github.com/users/"
-export const URL_SEARCH = 'https://api.github.com/search/users?q="'
-export const PARAMS_USER = '/repos?sort="pushed"&per_page=6';
-export const PARAMS_SEARCH = '"&sort="followers"&per_page=3'
+export const URL = "https://api.github.com/users/"
+export const PARAMS = '/repos?sort="pushed"&per_page=6';
+
